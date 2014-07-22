c61117f3069663c5635b49447617d65ee15787dc
Pastafarianist <mr.pastafarianist@gmail.com>
2014-07-23 01:35:02 +0400

    Initial solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# A straightforward implementation of a "caching matrix", i.e.
+# a storage for a matrix which also caches its inverse and computes it
+# upon request.
+#
+# This code follows the Google R Style Guide:
+# https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
+# In particular, the indentation is 2 spaces per level, in contrast
+# with what is used in the lectures.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+  # Makes a new "caching matrix".
+  #
+  # Args:
+  #   x: initial value of the matrix
+  #
+  # Returns:
+  #   A "caching matrix".
+  m <- x  # unless this is done, get() may return a circular reference
+  inv <- NULL
+  get <- function() m
+  set <- function(y) {
+    m <<- y
+    inv <<- NULL
+  }
+  getinv <- function() inv
+  setinv <- function(inverse) {
+    inv <<- inverse
+  }
+  list(get=get, set=set, getinv=getinv, setinv=setinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Computes the inverse of a "caching matrix", either by retrieving
+  # from cache or from scratch.
+  #
+  # Args:
+  #   x: a "caching matrix"
+  #
+  # Returns:
+  #   The inverse of x.
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    return(inv)
+  }
+  inverse <- solve(x$get())
+  x$setinv(inverse)
+  inverse
 }
